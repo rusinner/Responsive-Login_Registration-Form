a09319cae4fe5f1d89bfe5e4470a4a146a45dbee
Theologos Mavropoulos <rusinner88@gmail.com>
2022-10-03 21:24:42 +0300

    opacity sign-up-form needs fixed

---
+++ main.js
@@ -0,0 +1,14 @@
+//variables declaration
+const sign_in_btn = document.querySelector('#sign-in-btn');
+const sign_up_btn = document.querySelector('#sign-up-btn');
+const container = document.querySelector('.container');
+
+//add event listeners
+sign_up_btn.addEventListener('click', () =>{
+ container.classList.add('sign-up-mode');
+});
+
+sign_in_btn.addEventListener('click', () =>{
+    container.classList.remove('sign-up-mode');
+   });
+
